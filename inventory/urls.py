8e58ea95bb864a896d95ca2bae9b73173517a640
Diego Orellana <dorellang@gmail.com>
2018-07-29 18:50:43 -0400

    created page

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,11 @@
-from inventory.views import ProductViewSet
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
+
+from .views import ProductViewSet, IndexView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, base_name='product')
-urlpatterns = router.urls
+
+urlpatterns = [
+    url(r'^$', IndexView.as_view(), name="index")
+] + router.urls
--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.views.generic.base import TemplateView
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
@@ -9,3 +10,12 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+
+class IndexView(TemplateView):
+
+    template_name = "inventory/index.html"
+
+    # may be needed later
+    def get_context_data(self, **kwargs):
+        pass
