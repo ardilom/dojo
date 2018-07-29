c6612a297fdce9144e33d7f0704d9029251f66b5
Ardilom <fardila120@gmail.com>
2018-07-29 20:57:42 +0000

    inventory app

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('name', max_length=250)
+    quantity = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        "<product: {} -- qty: {}>".format(self.name, self.quantity)
